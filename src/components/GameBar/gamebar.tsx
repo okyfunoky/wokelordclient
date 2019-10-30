f8f45e16a9e1a254c186a8614095649bffe38bfa
Michael Stevens <okyfunoky@hotmail.com>
2019-10-30 08:08:18 -0700

    Starting the gamebar

--- src/components/GameBar/gamebar.tsx
+++ src/components/GameBar/gamebar.tsx
@@ -1,1 +1,33 @@
 import * as React from 'react';
+import './style.css';
+
+export interface GamebarProps {
+    text: string
+}
+
+// 'HelloProps' describes the shape of props.
+// State is never set so we use the '{}' type.
+export default class Tower extends React.Component<GamebarProps, {}> {
+    state = {
+        text: this.props.text
+    }
+
+    render() {
+        return (
+            <div className="gamebar">
+                <div className="money">
+
+                </div>
+                <div className="repuation">
+
+                </div>
+                <div className="stars">
+
+                </div>
+                <div className="trackedtenants">
+
+                </div>
+            </div>
+        )
+    }
+}
