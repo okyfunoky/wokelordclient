7838978fb2dced83f74d807eae87ca4c682a8abe
Michael Stevens <okyfunoky@hotmail.com>
2019-11-07 07:33:59 -0800

    Updates

---
+++ precache-manifest.5db22933a8a0a614a2c51699c90afa0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fded132c52cd16e188bdda57f151de1a",
+    "url": "/wokelordclient/index.html"
+  },
+  {
+    "revision": "0dd9faa666d84bb988db",
+    "url": "/wokelordclient/static/css/main.e8ff9fc0.chunk.css"
+  },
+  {
+    "revision": "3674b6b9276978f975b4",
+    "url": "/wokelordclient/static/js/2.a804f4fb.chunk.js"
+  },
+  {
+    "revision": "0dd9faa666d84bb988db",
+    "url": "/wokelordclient/static/js/main.6473af69.chunk.js"
+  },
+  {
+    "revision": "39ff303ca2959fe790a5",
+    "url": "/wokelordclient/static/js/runtime-main.59c3f623.js"
+  },
+  {
+    "revision": "0b169ba57fdadc1311bc7a9cf56115ed",
+    "url": "/wokelordclient/static/media/Crane-large-pixel-clipart-design-clipartsfree-removebg-preview.0b169ba5.png"
+  }
+]);
