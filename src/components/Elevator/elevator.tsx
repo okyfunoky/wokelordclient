211dd2f68c661fce3e91da622f4caf25f8d3254e
Michael Stevens <okyfunoky@hotmail.com>
2019-11-09 12:40:26 -0800

    Working elevator animation

--- src/components/Elevator/elevator.tsx
+++ src/components/Elevator/elevator.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import posed from 'react-pose'
 import './style.css';
 
 interface ElevatorProps {
@@ -6,13 +7,26 @@ interface ElevatorProps {
   position: string,
 }
 
-const Elevator: React.FC<ElevatorProps> = (props) => {
-  
-    return (
-    <div className={props.position==="left" ? "leftElevator" : "rightElevator"}>
-        <div className="elevatorCar">C</div>
-    </div>
-  );
-}
+const Box = posed.div({
+  hidden: { bottom: 0, transition: { duration: 5000 } },
+  visible: { bottom: 2500, transition: { duration: 5000 } }
+});
 
-export default Elevator;
+export default class Example extends React.Component<ElevatorProps> {
+  state = { isVisible: true };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ isVisible: !this.state.isVisible });
+    }, 10000);
+  }
+
+  render() {
+    const { isVisible } = this.state;
+    return (
+      <div className={this.props.position === "left" ? "leftElevator" : "rightElevator"}>
+        <Box className="elevatorCar" pose={isVisible ? 'visible' : 'hidden'} />
+      </div>
+    )
+  }
+}
